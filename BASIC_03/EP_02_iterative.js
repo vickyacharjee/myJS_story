bb7e31d458d382ba02e0bb33400001b42bcc706f
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-13 22:19:03 +0530

    more precise

--- BASIC_03/EP_02_iterative.js
+++ BASIC_03/EP_02_iterative.js
@@ -8,9 +8,9 @@
 
 
         //Ex
-        for (let i = 1; i < 10; i++) {
+        for (let i = 1; i <= 10; i++) {
    
-            for (let j = 1; j < 10; j++) {
+            for (let j = 1; j <=10; j++) {
               
                 console.log(i+"X"+j+"="+i*j);
                 
