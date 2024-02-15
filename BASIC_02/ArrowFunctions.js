d08ec3b4ce557582863cffa7898437e0b80f2cb0
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-15 13:45:54 +0530

    add Arrow Func

---
+++ BASIC_02/ArrowFunctions.js
@@ -0,0 +1,19 @@
+//Traditional Fucntion
+
+function name(x,y) {
+    return 2+3;
+}
+
+console.log(name(1,2));
+
+//Arrow Function
+
+const arrow=(x,y)=>x+y;
+console.log(arrow(1,2));//implicit
+
+const arrowE=(x,y)=>{
+    const res=x+y
+    return res;
+}
+console.log(typeof(arrowE(1,2)));
+console.log(arrowE(1,2));//Explicit
