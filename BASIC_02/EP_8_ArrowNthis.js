d173fdedfa76522cdb37b60dc729ab16ee21b664
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-24 23:16:04 +0530

    added for implicit N Explicit

--- BASIC_02/EP_8_ArrowNthis.js
+++ BASIC_02/EP_8_ArrowNthis.js
@@ -24,3 +24,31 @@ const obj={
 //     }
 // }
 obj.func()
+
+
+console.log("Arrow  functions");
+// Arrow functions
+
+const obj1=(n1,n2)=>{
+    return n1+n2;
+}
+
+console.log(obj1(1,2));
+
+//implicit Arrow Func
+
+const obj2=()=>console.log("hello from implicit");
+obj2()
+
+// const obj3=()=>(console.log("hello from implicit"))
+// obj3()
+
+//throws an undefined bcoz of not use of ()
+// const obj3=()=>{name:21}
+// console.log(obj3());
+
+//true, coz only use of arrow functions of object as ()
+const obj3=()=>({name:21})
+console.log(obj3());
+
+//Remeber explict means havong a use of return keyword as well consumptio  of mor more than 2 lines
