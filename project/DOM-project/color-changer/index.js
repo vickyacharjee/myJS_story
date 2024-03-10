884ff113c7eeea7f64edd85079cba6f044b60438
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-03-11 00:34:48 +0530

    added 2 project

--- project/DOM-project/color-changer/index.js
+++ project/DOM-project/color-changer/index.js
@@ -1,6 +1,8 @@
 const buttons=document.querySelectorAll('.button');
 const body=document.querySelector('body');
 
+const colorChanger=document.querySelector('.colorChanger');
+
 
 buttons.forEach((button)=>{
     console.log(button);
@@ -18,5 +20,21 @@ buttons.forEach((button)=>{
         if(e.target.id=="brown"){
             body.style.backgroundColor=e.target.id;
         }
+    })
+    button.addEventListener('mouseover',(e)=>{
+        console.log(e.target);
+        if(e.target.id=="red"){
+            colorChanger.innerHTML=e.target.id
+        }
+       if(e.target.id=="pink"){
+            colorChanger.innerHTML=e.target.id
+        }
+       if(e.target.id=="brown"){
+            colorChanger.innerHTML=e.target.id
+        }
+       if(e.target.id=="yellow"){
+            colorChanger.innerHTML=e.target.id
+        }
+       
     })
 })
