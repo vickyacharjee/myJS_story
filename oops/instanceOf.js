0d29c2c355e32042c198de3e832c744829febe50
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-04-01 15:08:22 +0530

    added for .bind()

--- oops/instanceOf.js
+++ oops/instanceOf.js
@@ -1,5 +1,5 @@
 // The instanceof operator tests to see if the prototype property of a constructor 
-//appears anywhere in the prototype chain of an object.
+// appears anywhere in the prototype chain of an object.
 //  The return value is a boolean value. Its behavior can be customized with Symbol.hasInstance
 
 
